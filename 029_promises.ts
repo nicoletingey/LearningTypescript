bb76125199cfb82ea91a2f7d28f59d83df0576f7
Nicole Tingey <nicole.tingey1@gmail.com>
2016-11-11 10:33:57 -0700

    promises in typescript

---
+++ 029_promises.ts
@@ -0,0 +1,33 @@
+"use strict";
+
+// Start mowing -> Pending
+// Complete mowing process -> Resolve
+// Did not complete mowing process -> Reject 
+//(sending out an api request and getting back an error^)
+
+let performUpload = function(imgStatus : string) : Promise<{imgStatus : string}> {
+	return new Promise((resolve) => {
+		console.log(`Status: ${imgStatus}`);
+		setTimeout(() => {
+			resolve({ imgStatus : imgStatus });
+		}, 1000);
+	});
+}
+
+var upload;
+var compress;
+var transfer;
+ 
+performUpload('uploading...')
+.then((res) => {
+	upload = res;
+	return performUpload('compressing...');
+})
+.then((res) => {
+	compress = res;
+	return performUpload('transfering...');
+})
+.then((res) => {
+	transfer = res;
+	return performUpload('Image upload completed.');
+});
